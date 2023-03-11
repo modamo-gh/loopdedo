9fbfc45c5c5e53510cf5d9fa5ffd09495b4e6c56
Bryan <morganthemosaic@gmail.com>
2023-03-10 22:11:25 -0500

    Display an inputted task

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const tasks = [];
+const submitButton = document.querySelector("#submitButton");
+const taskInput = document.querySelector("#task");
+
+const currentTask = document.createElement("p");
+let textNode = document.createTextNode("");
+currentTask.appendChild(textNode);
+
+const nextButton = document.createElement("button");
+nextButton.textContent = "NEXT";
+
+let currentTaskIndex = 0;
+
+submitButton.addEventListener("click", () => {
+    tasks.push(taskInput.value);
+
+    if(tasks.length === 1){
+        textNode.textContent = tasks[currentTaskIndex];
+        body.appendChild(nextButton);
+    }
+})
+
+const body = document.querySelector("body");
+body.appendChild(currentTask);
