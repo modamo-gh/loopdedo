2218435ceb418ba8672d2d6cfc0c47962e549edd
Bryan <morganthemosaic@gmail.com>
2023-03-12 16:04:29 -0400

    Add displayed task and button to own div

--- js/script.js
+++ js/script.js
@@ -12,16 +12,20 @@ nextButton.textContent = "NEXT";
 let currentTaskIndex = 0;
 const container = document.querySelector(".container");
 
+const taskDiv = document.createElement("div")
+taskDiv.classList.add("taskDiv");
+
 submitButton.addEventListener("click", () => {
     tasks.push(taskInput.value);
 
     if(tasks.length === 1){
         textNode.textContent = tasks[currentTaskIndex];
-        container.appendChild(nextButton);
+        taskDiv.appendChild(nextButton);
     }
 })
 
-container.appendChild(currentTask);
+taskDiv.appendChild(currentTask);
+container.appendChild(taskDiv);
 
 nextButton.addEventListener("click", () => {
     currentTaskIndex = (currentTaskIndex + 1) % tasks.length;
